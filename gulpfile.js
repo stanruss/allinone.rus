db108127635498392e2366d7a4dbfd37d9aeb8a3
stanruss <stan19781@gmail.com>
2019-02-08 13:32:24 +0300

    gulp 4 add

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,6 @@
+var syntax        = 'sass', // Syntax: sass or scss;
+		gulpversion   = '4'; // Gulp version: 3 or 4
+
 var gulp         = require('gulp');
 var	rename       = require('gulp-rename');
 var	postcss      = require('gulp-postcss');
@@ -29,7 +32,7 @@ gulp.task('sass', function() {
 				relativeTo: 'assets/templates/Stas/app/css/'
 			})
 		];
-	return gulp.src('assets/templates/Stas/app/sass/**/*.sass')
+	return gulp.src('assets/templates/Stas/app/'+syntax+'/**/*.'+syntax+'')
 	.pipe(sass().on("error", notify.onError()))
 	.pipe(postcss(processors))
 	.pipe(rename({suffix: '.min', prefix : ''}))
@@ -41,8 +44,21 @@ gulp.task('sass', function() {
 	.pipe(gulp.dest('assets/templates/Stas/app/css'))
 	.pipe(browserSync.reload({stream: true}));
 });
+gulp.task('code', function() {
+	return gulp.src('app/*.html')
+	.pipe(browserSync.reload({ stream: true }))
+});
+if (gulpversion == 3) {
 gulp.task('watch', ['sass', 'browser-sync'], function() {
-	gulp.watch('assets/templates/Stas/app/sass/**/*.sass', ['sass']);
+	gulp.watch('assets/templates/Stas/app/'+syntax+'/**/*.'+syntax+'', ['sass']);
 	gulp.watch('*.php', browserSync.reload);
 });
 gulp.task('default', ['watch', 'sass']);
+}
+if (gulpversion == 4) {
+	gulp.task('watch', function() {
+	gulp.watch('assets/templates/Stas/app/'+syntax+'/**/*.'+syntax+'', gulp.parallel('sass'));
+	gulp.watch('app/*.html', gulp.parallel('code'))
+});
+gulp.task('default', gulp.parallel('sass', 'browser-sync', 'watch'));
+}
